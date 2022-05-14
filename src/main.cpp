3ce5bd6f853d44e6f9798d2e9faecf5a02bfae71
marques-bruno <bmarques68@gmail.com>
2022-05-14 22:01:08 +0200

    ADD main.cpp draft
    - Move compile_commands.json to sources
    - Add .clangd/ and compile_commands.json to gitignore
    - Update README
    - add spdlog and fmt to dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
-#include <boost/assert.hpp>
 #include <CLI/CLI.hpp>
+#include <spdlog/spdlog.h>
 
-int main(void)
+int main(int ac, char** av)
 {
-  // BOOST_ASSERT(42);
-  // return 0;
+  try {
+    CLI::App app{ fmt::format("{} version {}", "myproject", "0.1") };
+
+    std::optional<std::string> message;
+    app.add_option("-m,--message", message, "A message to print back out");
+    bool show_version = false;
+    app.add_flag("--version", show_version, "Show version information");
+
+    CLI11_PARSE(app, ac, av);
+
+    if (show_version) {
+      fmt::print("{}\n", "0.1");
+      return EXIT_SUCCESS;
+    }
+
+    spdlog::info("Hellow World! :)");
+    
+    if (message) {
+      fmt::print("Message: '{}'\n", *message);
+    } else {
+      fmt::print("No Message Provided :(\n");
+    }
+  } catch (const std::exception &e) {
+    spdlog::error("Unhandled exception in main: {}", e.what());
+  }
 }
